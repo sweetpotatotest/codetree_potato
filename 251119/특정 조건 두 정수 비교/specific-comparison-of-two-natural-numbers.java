de4af16c3b0e49bb7d9ae10fc4672003a075607d
sweetpotatotest <151900619+sweetpotatotest@users.noreply.github.com>
2025-11-19 15:51:36 +0900

    Title: 특정 조건 두 정수 비교, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a < b) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
+        }
+
+        if(a == b) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
