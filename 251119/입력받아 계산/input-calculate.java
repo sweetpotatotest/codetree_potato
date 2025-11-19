71a8ca262297dbf02d5957e7946df575ddfec4af
sweetpotatotest <151900619+sweetpotatotest@users.noreply.github.com>
2025-11-19 14:55:20 +0900

    Title: 입력받아 계산, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/입력받아 계산/input-calculate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a <= 100 && a >= 1) {
+            System.out.println(a+2);
+        }
+    }
+}
