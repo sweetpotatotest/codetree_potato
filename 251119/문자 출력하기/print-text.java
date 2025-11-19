559dadd35556dad14c6b5e411f844fd2059ab481
sweetpotatotest <151900619+sweetpotatotest@users.noreply.github.com>
2025-11-19 17:18:56 +0900

    Title: 문자 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/문자 출력하기/print-text.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i=0; i<8; i++){
+            System.out.print(a);
+        }
+    }
+}
